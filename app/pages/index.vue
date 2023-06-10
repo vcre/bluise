66ec04b367aa5d715e06f1bebbc312b68945840a
it <96355727+vcre@users.noreply.github.com>
2023-06-10 14:29:46 +0700

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -37,7 +37,7 @@
         <img
           alt="Hero"
           class="rounded shadow-xl"
-          src="https://source.unsplash.com/random/720x400"
+          src="https://static.thichcay.com/thuduc.jpg"
         />
       </div>
     </div>
